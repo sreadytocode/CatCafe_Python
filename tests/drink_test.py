0295dbe9a787caa4398effe57f42b59979b0d37b
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 20:45:35 +0000

    Wrote unittest skip on second test

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -6,5 +6,6 @@ class TestDrink(unittest.TestCase):
     def test_does_drink_has_a_name(self):
         self.assertEqual("Latte", self.drink.name)
 
+    @unittest.skip("WIP")
     def test_does_drink_has_a_price(self):
         self.assertEquals(3.20, self.drink.price)
