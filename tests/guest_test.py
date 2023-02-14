f37b1497ee8e8b18944d8b93b99350d9e47c6cf1
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 19:54:41 +0000

    Wrote first test tdd for guest

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,4 +4,4 @@ from classes.guest import Guest
 class TestGuest(unittest.TestCase):
     
     def test_does_guest_have_a_name(self):
-        pass
+        self.assertEqual("Sara", self.guest.name)
