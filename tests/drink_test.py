f72b51d480bec493e48bbb8cf9dcfde0ff756418
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 20:47:20 +0000

    Added setup to drink test

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -1,7 +1,10 @@
 import unittest
+from classes.drink import Drink
 
 class TestDrink(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.drink = Drink()
 
     def test_does_drink_has_a_name(self):
         self.assertEqual("Latte", self.drink.name)
