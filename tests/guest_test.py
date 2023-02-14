cc9e173c00f48410797ae02c9db25ded4c0373ee
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 20:06:20 +0000

    Added setup in guest test file

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,6 +2,9 @@ import unittest
 from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
+
+    def setUp(self):
+        self.guest = Guest("Alice", "Liddell", 50.00)
     
     def test_does_guest_have_a_first_name(self):
         self.assertEqual("Alice", self.guest.first_name)
