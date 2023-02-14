940fb8f822ff426fb46df36f04377ec9a1615a63
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 19:56:00 +0000

    Refactured test

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,5 +3,5 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
     
-    def test_does_guest_have_a_name(self):
-        self.assertEqual("Sara", self.guest.name)
+    def test_does_guest_have_a_first_name(self):
+        self.assertEqual("Sara", self.guest.first_name)
