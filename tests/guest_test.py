ea8c7d0ed02b55a4b08af9bf8fc682f8cacff94b
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 19:59:26 +0000

    Wrote 2 more tests for guest

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,4 +4,10 @@ from classes.guest import Guest
 class TestGuest(unittest.TestCase):
     
     def test_does_guest_have_a_first_name(self):
-        self.assertEqual("Sara", self.guest.first_name)
+        self.assertEqual("Alice", self.guest.first_name)
+
+    def test_does_guest_have_a_last_name(self):
+        self.assertEqual("Liddell", self.guest.last_name)
+
+    def test_does_guest_have_cash(self):
+        self.assertEqual(50.0, self.guest.cash)
