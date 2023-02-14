8b62a89250c5d0caecfc4f5191d2ff6c3fddd2f7
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 21:13:09 +0000

    Added code so passed test

--- classes/cat.py
+++ classes/cat.py
@@ -1,4 +1,5 @@
 class Cat:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, appetite):
         self.first_name = first_name
         self.last_name = last_name
+        self.appetite = appetite
