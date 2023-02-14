3fe97bd2113eab58bc5c44a3265d79609b69669e
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 20:12:58 +0000

    Wrote code so test for wallet is passing

--- classes/guest.py
+++ classes/guest.py
@@ -1,5 +1,6 @@
 class Guest:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, wallet):
         self.first_name = first_name
         self.last_name = last_name
+        self.wallet = wallet
         
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,7 +4,7 @@ from classes.guest import Guest
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest = Guest("Alice", "Liddell")
+        self.guest = Guest("Alice", "Liddell", 50.00)
     
     def test_does_guest_have_a_first_name(self):
         self.assertEqual("Alice", self.guest.first_name)
@@ -13,6 +13,6 @@ class TestGuest(unittest.TestCase):
     def test_does_guest_have_a_last_name(self):
         self.assertEqual("Liddell", self.guest.last_name)
 
-    @unittest.skip("WIP")
+    # @unittest.skip("WIP")
     def test_does_guest_have_cash(self):
         self.assertEqual(50.00, self.guest.wallet)
