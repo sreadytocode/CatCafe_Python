4d90bf24fc1bd1a6803845d320158a67876265f1
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 20:52:22 +0000

    Added price to setup in test drink

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -4,11 +4,11 @@ from classes.drink import Drink
 class TestDrink(unittest.TestCase):
     
     def setUp(self):
-        self.drink = Drink("Latte")
+        self.drink = Drink("Latte", 3.20)
 
     def test_does_drink_has_a_name(self):
         self.assertEqual("Latte", self.drink.name)
 
-    @unittest.skip("WIP")
+    # @unittest.skip("WIP")
     def test_does_drink_has_a_price(self):
         self.assertEquals(3.20, self.drink.price)
