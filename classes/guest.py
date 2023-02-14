0a8f531c396fea30db721b592e15b715c98604ad
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 20:09:16 +0000

    Added unittest skips to skip other tests then wrote code to ensure first test passing

--- classes/guest.py
+++ classes/guest.py
@@ -1,3 +1,4 @@
 class Guest:
-    pass
+    def __init__(self, first_name):
+        self.first_name = first_name
         
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,13 +4,15 @@ from classes.guest import Guest
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest = Guest("Alice", "Liddell", 50.00)
+        self.guest = Guest("Alice")
     
     def test_does_guest_have_a_first_name(self):
         self.assertEqual("Alice", self.guest.first_name)
 
+    @unittest.skip("WIP")
     def test_does_guest_have_a_last_name(self):
         self.assertEqual("Liddell", self.guest.last_name)
 
+    @unittest.skip("WIP")
     def test_does_guest_have_cash(self):
         self.assertEqual(50.00, self.guest.wallet)
