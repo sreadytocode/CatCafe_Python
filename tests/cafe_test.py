8cb40915bbc72b65a7eb149275211624190e51ed
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-15 14:14:07 +0000

    Added new test that passes

--- tests/cafe_test.py
+++ tests/cafe_test.py
@@ -9,6 +9,7 @@ class TestCafe(unittest.TestCase):
         self.drink_2 = Drink("Earl Grey Tea", 3.50)
 
         self.cafe = Cafe("Wonderland", 500.00)
+        
 
     def test_does_cafe_have_a_name(self):
         self.assertEqual("Wonderland", self.cafe.name)
@@ -30,4 +31,10 @@ class TestCafe(unittest.TestCase):
         self.cafe.add_drink(self.drink_2)
         self.assertEqual(1, self.cafe.drink_stock_level(self.drink_2))
         self.assertEqual(3.50, self.cafe.drink_stock_value())
+
+    def test_cafe_can_add_multiple_drinks(self):
+        self.cafe.add_drink(self.drink_1)
+        self.cafe.add_drink(self.drink_1)
+        self.assertEqual(2, self.cafe.drink_stock_level(self.drink_1))
+        self.assertEqual(7.00, self.cafe.drink_stock_value())
 
